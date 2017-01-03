42b0c7baf5b9daa3488448628b319858606b29bc
hania syed <hania.syed@aiesec.net>
2017-01-03 16:23:32 +0100

    updated database structure

--- database/collections.js
+++ database/collections.js
@@ -2,3 +2,10 @@ import { Mongo } from 'meteor/mongo';
  
 export const TransportVehicles = new Mongo.Collection('transportVehicles');
 export const Respberries = new Mongo.Collection('respberries');
+export const Seats = new Mongo.Collection('seats'); 
+
+// TransportVehicles structure
+// {
+//    "vehicleId": "",
+    
+// }
