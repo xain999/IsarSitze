0de152f4b67860ca601f6a84cf90e4545fdfaea8
Zain Ul Abideen <zain.ul.abideen@tum.de>
2017-06-01 00:33:35 +0200

    project updated, and DEPLOY Flag added

--- scripts/rasp/mockPublisher.py
+++ scripts/rasp/mockPublisher.py
@@ -11,6 +11,14 @@ import paho.mqtt.client as mqtt
 from pymongo import MongoClient
 
 WEB_ADDRESS = 'http://localhost:3000'
+MQTT_ADDRESS = 'localhost'
+MONGO_ADDRESS = 'mongodb://127.0.0.1:3001/meteor'
+DEPLOY = True
+
+if DEPLOY:
+    WEB_ADDRESS = 'http://live.travis-mobility.com'
+    MQTT_ADDRESS = 'mqtt.travis-mobility.com'
+    MONGO_ADDRESS = 'mongodb://travis:thisiszain@mongo.travis-mobility.com:27017/travis'
 
 """
 The callback for when the client receives a CONNACK response from the server.
@@ -64,14 +72,19 @@ mqttClient = mqtt.Client()
 mqttClient.on_connect = onConnect
 mqttClient.on_message = onMessage
 
-mqttClient.connect('localhost')
+mqttClient.connect(MQTT_ADDRESS)
 #mqtt.loop_start()
 
-mongoClient = MongoClient('mongodb://127.0.0.1:3001/meteor')
+mongoClient = MongoClient(MONGO_ADDRESS)
 
 print (mongoClient.database_names())
+
 VehiclesDB = mongoClient.meteor.transportVehicles
 RaspberriesDB = mongoClient.meteor.raspberries
+
+if DEPLOY:
+    VehiclesDB = mongoClient.travis.transportVehicles
+    RaspberriesDB = mongoClient.travis.raspberries
 
 urls = []
 raspberries = []
